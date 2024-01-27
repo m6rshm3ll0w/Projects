95d7e2f542eff6102c3022738a9fa51b97fa6245
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2024-01-27 11:15:14 +0300

    исправленны ошибки

--- snake/Program.cs
+++ snake/Program.cs
@@ -12,6 +12,7 @@ using System.Xml.Linq;
 
 Console.CursorVisible = false;
 
+// инициализация 
 var rand = new Random();
 int x = 25;
 int y = 16;
@@ -22,17 +23,18 @@ int yc = y / 2;
 int apple = 0;
 int snake_len = 1;
 int max_ap = 1;
-
+int death = 0;
 char csimb = 'x';
 char cspas = 'x';
 char applsymb = '@';
-string axiz = "y-";
+//string axiz = "y-";
 int speed = 5;
 var keyd = ConsoleKey.UpArrow;
 List<int> sx = new List<int>() { x / 2, x / 2 };
 List<int> sy = new List<int>() { y / 2, y / 2 };
-List<int> ax = new List<int>() { x / 2, x / 2 };
-List<int> ay = new List<int>() { y / 2, y / 2 };
+//List<int> ax = new List<int>() { x / 2, x / 2 };
+//List<int> ay = new List<int>() { y / 2, y / 2 };
+
 
 
 
@@ -45,60 +47,65 @@ Console.SetBufferSize(width: x, height: y);
 Console.SetCursorPosition(xc, yc);
 Console.Write(csimb);
 
-while (true)
+
+while (death != 1)
 {
 
-    if (axiz == "y-")
-    {
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(cspas);
+    //// ↓ автоматическое движение по осям
+    //if (axiz == "y-")
+    //{
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(cspas);
 
-        yc--;
-        if (yc < 0) yc = 0;
+    //    yc--;
+    //    if (yc < 0) { yc = 0; death = 1;  }
 
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(csimb);
-    }
-    else if (axiz == "y+")
-    {
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(cspas);
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(csimb);
+    //}
+    //else if (axiz == "y+")
+    //{
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(cspas);
 
-        yc++;
-        if (yc > y - 1) yc = y - 1;
+    //    yc++;
+    //    if (yc > y - 1) {yc = y - 1; death = 1; }
 
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(csimb);
-    }
-    else if (axiz == "x-")
-    {
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(cspas);
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(csimb);
+    //}
+    //else if (axiz == "x-")
+    //{
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(cspas);
 
-        xc--;
-        if (xc < 0) xc = 0;
+    //    xc--;
+    //    if (xc < 0) {xc = 0; death = 1; }
 
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(csimb);
-    }
-    else if (axiz == "x")
-    {
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(cspas);
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(csimb);
+    //}
+    //else if (axiz == "x+")
+    //{
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(cspas);
 
-        xc++;
-        if (xc > x - 1) xc = x - 1;
+    //    xc++;
+    //    if (xc > x - 1) {xc = x - 1; death = 1; }
 
-        Console.SetCursorPosition(xc, yc);
-        Console.Write(csimb);
-    }
-    //Console.SetCursorPosition(1, 1);
-    //Console.Write($"{snake_len},{axiz}");
+    //    Console.SetCursorPosition(xc, yc);
+    //    Console.Write(csimb);
+    //}
+    //// ↑ автоматическое движение по осям
 
-    ax.Add(xc);
-    ay.Add(yc);
+    ////Console.SetCursorPosition(1, 1); // вывод счета
+    ////Console.Write($"{snake_len},{axiz}");
 
-    if (apple < max_ap) //tab
+    //sx.Add(xc); // запись авто перемешения по X
+    //sy.Add(yc); // запись авто перемешения по Y
+
+    // генерация яблок
+    if (apple < max_ap) 
     {
         axr = rand.Next(0, x);
         ayr = rand.Next(0, y);
@@ -108,82 +115,95 @@ while (true)
         apple++;
     }
 
-
+    // ↓ считывание нажатия
     keyd = Console.ReadKey().Key;
+    // ↑ считывание нажатия
 
-    Thread.Sleep(speed);
-    if (keyd == ConsoleKey.UpArrow && axiz != "y-" && axiz != "y+") //  y_up
+    //Thread.Sleep(speed); // это должна быть задержка
+
+    //  проверка на нажатие клавиши вниз (y-- - up)
+    if (keyd == ConsoleKey.UpArrow /*&& axiz != "y-" && axiz != "y+"*/) 
     {
         Console.SetCursorPosition(xc, yc);
         Console.Write(cspas);
 
         yc--;
-        if (yc < 0) yc = 0;
+        if (yc < 0) {yc = 0; death = 1; }
 
         Console.SetCursorPosition(xc, yc);
         Console.Write(csimb);
-        axiz = "y-";
+        //axiz = "y-";
     }
-    else if (keyd == ConsoleKey.DownArrow && axiz != "y-" && axiz != "y+") //  y_down
+
+    //  проверка на нажатие клавиши вниз (y++ - down)
+    else if (keyd == ConsoleKey.DownArrow /*&& axiz != "y-" && axiz != "y+"*/) 
     {
         Console.SetCursorPosition(xc, yc);
         Console.Write(cspas);
 
         yc++;
-        if (yc > y - 1) yc = y - 1;
+        if (yc > y - 1) {yc = y - 1; death = 1; }
 
         Console.SetCursorPosition(xc, yc);
         Console.Write(csimb);
-        axiz = "y+";
+        //axiz = "y+";
 
     }
-    else if (keyd == ConsoleKey.LeftArrow && axiz != "x-" && axiz != "x+") //  x_left
+
+    //  проверка на нажатие клавиши влево (x-- - down)
+    else if (keyd == ConsoleKey.LeftArrow /*&& axiz != "x-" && axiz != "x+"*/) 
     {
         Console.SetCursorPosition(xc, yc);
         Console.Write(cspas);
 
         xc--;
-        if (xc < 0) xc = 0;
+        if (xc < 0) {xc = 0; death = 1; }
 
         Console.SetCursorPosition(xc, yc);
         Console.Write(csimb);
-        axiz = "x-";
+        //axiz = "x-";
 
     }
-    else if (keyd == ConsoleKey.RightArrow && axiz != "x-" && axiz != "x+") //  x_right
+
+    //  проверка на нажатие клавиши право (x++ - down)
+    else if (keyd == ConsoleKey.RightArrow /*&& axiz != "x-" && axiz != "x+"*/) 
     {
         Console.SetCursorPosition(xc, yc);
         Console.Write(cspas);
 
         xc++;
-        if (xc > x - 1) xc = x - 1;
+        if (xc > x - 1) {xc = x - 1; death = 1; }
 
         Console.SetCursorPosition(xc, yc);
         Console.Write(csimb);
-        axiz = "x+";
+        //axiz = "x+";
 
     }
 
-
-    if (axr == xc && ayr == yc)
+    // проверка на сьедение яблок
+    if (axr == xc && ayr == yc) 
     {
         snake_len++;
         apple--;
     }
 
 
+    sx.Add(xc); // запись перемешения по X 
+    sy.Add(yc); // запись перемешения по Y
 
-    sx.Add(xc);
-    sy.Add(yc);
-
+    // ↓ получение текущих координат
     int ccx = sx[sx.Count - 1 - snake_len];
     int ccy = sy[sy.Count - 1 - snake_len];
-    int acx = ax[ax.Count - 1 - snake_len];
-    int acy = ay[ay.Count - 1 - snake_len];
+    //int acx = ax[ax.Count - 1 - snake_len];
+    //int acy = ay[ay.Count - 1 - snake_len];
+    // ↑ получение текущих координат
 
-    Console.SetCursorPosition(ccx, ccy);
+    Console.SetCursorPosition(ccx, ccy); // отчистка ранне закрашенного поля
     Console.Write(" ");
-    Console.SetCursorPosition(acx, acy);
-    Console.Write(" ");
+    //Console.SetCursorPosition(acx, acy); // отчистка ранне закрашенного поля
+    //Console.Write(" ");
 
 }
+Console.Clear(); 
+Console.WriteLine("Death!");
+Console.ReadKey();
